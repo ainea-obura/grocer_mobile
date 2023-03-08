87d41f0a57a64fa7bc69711289fa5c98bfd7278d
ainea-obura <anandaobura69@gmail.com>
2023-03-08 13:18:50 +0300

    reduced padding

--- lib/widgets/categories_widget.dart
+++ lib/widgets/categories_widget.dart
@@ -19,8 +19,9 @@ class CategoriesWidget extends StatelessWidget {
           return Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              //const SizedBox(height: 3,),
               Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(4.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -46,7 +47,7 @@ class CategoriesWidget extends StatelessWidget {
                 ),
               ),
               SizedBox(
-                height: 50.0,
+                height: 35.0,
                 child: ListView.builder(
                   scrollDirection: Axis.horizontal,
                   itemCount: categories.categories.length + 1,
@@ -92,80 +93,3 @@ class CategoriesWidget extends StatelessWidget {
     );
   }
 }
-
-
-
-
-
-
-// class CategoriesWidget extends StatelessWidget {
-//   const CategoriesWidget({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocBuilder<CategoriesCubit, CategoriesState>(
-//       builder: (context, state) {
-//         if (state is InitCategoryState || state is LoadingCategoryState) {
-//           return const Center(child: CircularProgressIndicator());
-//         } else if (state is SuccessCategoryState) {
-//           final categories = state.categories;
-//           return Column(
-//             crossAxisAlignment: CrossAxisAlignment.start,
-//             children: [
-//               Padding(
-//                 padding: const EdgeInsets.all(8.0),
-//                 child: Row(
-//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                   children: [
-//                     Text(
-//                       'Categories',
-//                       style: Theme.of(context).textTheme.titleLarge,
-//                     ),
-//                     TextButton(
-//                       onPressed: () {
-//                         // Handle "See All" button tap
-//                       },
-//                       child: const Text(
-//                         'See All',
-//                         style: TextStyle(
-//                           color: primaryColor,
-//                           fontWeight: FontWeight.bold,
-//                         ),
-//                       ),
-//                     ),
-//
-//                   ],
-//                 ),
-//               ),
-//               SizedBox(
-//                 height: 50.0,
-//                 child: ListView.builder(
-//                   scrollDirection: Axis.horizontal,
-//                   itemCount: categories.categories.length,
-//                   itemBuilder: (context, index) {
-//                     final category = categories.categories[index];
-//                     return GestureDetector(
-//                       onTap: () {
-//                         // Handle category tap
-//                       },
-//                       child: Padding(
-//                         padding: const EdgeInsets.symmetric(horizontal: 8.0),
-//                         child: Chip(
-//                           label: Text(category.title),
-//                         ),
-//                       ),
-//                     );
-//                   },
-//                 ),
-//               ),
-//             ],
-//           );
-//         } else if (state is ErrorCategoryState) {
-//           return Center(child: Text(state.message));
-//         }
-//         return const Center(child: Text('Unknown error'));
-//       },
-//     );
-//   }
-//
-// }
